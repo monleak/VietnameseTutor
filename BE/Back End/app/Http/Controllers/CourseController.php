ecbfea9fb4acf87f571217e4c6eeb26e336ea63a
monleak <hieu16112001@gmail.com>
2023-06-06 00:07:05 +0700

    first commit

---
+++ BE/Back End/app/Http/Controllers/CourseController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CreateCourseRequest;
+use App\Http\Requests\UpdateCourseRequest;
+use Illuminate\Http\Request;
+
+class CourseController extends Controller
+{
+    // Course table
+    // id		
+    // name		
+    // level		
+    // method		
+    // description		
+    // price		
+    // id_teacher		
+    // created_at		
+    // updated_at		
+
+    public function show($id){
+        $course = Course::find($id);
+        return $course;
+    }
+    public function list(Request $request){
+        $data = Course::all();
+        return $data;
+    }
+
+    public function create(CreateCourseRequest $request){
+        $course = new Course;
+        $course->name = $request->name;
+        $course->level = $request->level;
+        $course->method = $request->method;
+        $course->description = $request->description ? $request->description : null;
+        $course->price = $request->price;
+        $course->id_teacher = $request->id_teacher;
+        $course->save();
+        return $course;
+    }
+
+    public function update(UpdateCourseRequest $request){
+        $course = Course::find($request->id);
+        $course->name = $request->name;
+        $course->level = $request->level;
+        $course->method = $request->method;
+        $course->description = $request->description ? $request->description : null;
+        $course->price = $request->price;
+        $course->id_teacher = $request->id_teacher;
+        $course->save();
+
+        return $course;
+    }
+
+    public function destroy($id){
+        $course = Course::find($id);
+        $course->delete();
+
+        return "done";
+    }
+}
---
+++ BE/Back End/app/Http/Requests/CreateCourseRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateCourseRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required'],
+            'level' => ['required'],
+            'method' => ['required'],
+            'price' => ['required'],
+            'id_teacher' => ['required'],
+        ];
+    }
+}
